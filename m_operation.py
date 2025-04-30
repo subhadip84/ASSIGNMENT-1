77aa4b3785995788f83234b9a496b2487b58f414
subhadip84 <subhadip.bagchi@gmail.com>
2025-04-30 16:42:28 +0530

    Add files via upload

---
+++ m_operation.py
@@ -0,0 +1,11 @@
+# Takes two number from the user
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+a=int(a)
+b=int(b)
+# Perform basic arithmatic function
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition:",add,"\nSubtraction:",sub,"\nMultiplication:",mul,"\nDivision:",div)
---
+++ p_greeting.py
@@ -0,0 +1,10 @@
+# Input two string from user
+a1=input("Enter your first name: ")
+a2=input("Enter your last name: ")
+
+# concatenate two user input
+a3='!'
+a4=a1+" "+a2+a3
+
+# print the desired output 
+print("Hello,",a4,"Welcome to the Python program.")
